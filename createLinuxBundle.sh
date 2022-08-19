f38eebfbdfd179ff84b000312b71e57993dd462d
a.zinoviev <a.zinoviev@sb117.ru>
2022-08-19 14:18:06 +0700

    add

---
+++ createLinuxBundle.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./makeself-2.4.2.run
+./makeself-2.4.2/makeself.sh pkg converter "converter 264 to mp4" ./main.sh
+
---
+++ pkg/main.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+patch=$1
+tmpDirectory=$2
+ffmpegPatch=$3
+outputPatch=$4
+isAudio=$5
+
+inputBasename=`basename "$patch"`
+
+if [ -z "$patch" ];then
+  echo "Usage: converter patch_to_file.264"
+  exit 1
+fi
+
+if [ -z "$tmpDirectory" ];then
+  echo "Usage tmpDirectory: converter file.264 /tmp_stream"
+  exit 1
+fi
+
+if [ -z "$ffmpegPatch" ];then
+  echo "Usage ffmpegPatch: converter file.264 /tmp_stream /usr/bin/ffmpeg4"
+  exit 1
+fi
+
+if [ -z "$outputPatch" ];then
+  echo "Usage ffmpegPatch: converter file.264 /tmp_stream /usr/bin/ffmpeg4 output.mp4"
+  exit 1
+fi
+
+if [ -z "$isAudio" ];then
+  echo "Usage ffmpegPatch: converter file.264 /tmp_stream /usr/bin/ffmpeg4 output.mp4 1/0"
+  exit 1
+fi
+
+if [ ! -f "$patch" ]; then
+  echo "File not found: $patch"
+  exit 1
+fi
+
+tmpDir=$(mktemp -p $tmpDirectory --directory ci-XXXXXXXXXXXXXXXX)
+
+cp "$patch" "$tmpDir"/input.264
+cd $tmpDir
+
+ls -lh
+
+./convert2 input.264
+
+if [ "$isAudio" -eq "1" ]; then
+  $ffmpegPatch input.mp3 -i input.wav
+  ./mkvmerge  --output "input.mkv" --timestamps "0:input.video.ts.txt" "input.h264" "input.mp3"
+  $ffmpegPatch -i input.mkv -c:v copy -c:a copy -strict -1 output.mp4
+else
+  ./mkvmerge  --output "input.mkv" --timestamps "0:input.video.ts.txt" "input.h264"
+  $ffmpegPatch -i input.mkv -c:v copy -an output.mp4
+fi
+
+mv output.mp4 ${outputPatch}
+rm -rf $tmpDir
